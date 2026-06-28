e8c533bdb0968e48e6bc90ec70a39c4a1f6db4e3
Thisurika <donthisurika@gmail.com>
2026-06-28 22:53:48 +0530

    update deployment config

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,13 +1,27 @@
 import express from  "express";
+import path from "path";
 import {ENV} from "./lib/env.js";
 
 const app = express();
 
-console.log(process.env.PORT);
-console.log(process.env.DB_URL);
+const __dirname = path.resolve();
 
-app.get("/", (req, res) => {
+
+app.get("/sliit", (req, res) => {
     res.status(200).json({msg : "Welcome to SLIIT-IQ Backend"})
 });
+
+app.get("/it", (req, res) => {
+    res.status(200).json({msg : "This is the data science endpoint"})
+});
+
+//make sliit-iq app ready for deployment
+if(ENV.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+    app.get("/{*any}", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend","dist","index.html"));
+    });
+}
 
 app.listen(3000, () => console.log("Server is running on port 3000"));

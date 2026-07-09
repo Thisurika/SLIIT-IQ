0dddd8213804340616e741bca4c36458d0d048df
Thisurika <donthisurika@gmail.com>
2026-07-09 14:57:08 +0530

    server side auth implemented

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,10 +2,13 @@ import express from "express";
 import path from "path";
 import cors from "cors";
 import { serve } from "inngest/express";
+import { clerkMiddleware } from "@clerk/express";
 
 import { ENV } from "./lib/env.js";
 import { connectDB } from "./lib/db.js";
 import { inngest, functions } from "./lib/inngest.js";
+import {protectRoute} from "./middleware/protectRoute.js";
+import chatRoutes from "./routes/chatRoutes.js";
 
 
 const app = express();
@@ -16,8 +19,10 @@ const __dirname = path.resolve();
 app.use(express.json())
 // Server allows browser to include cookies on request
 app.use(cors({origin:ENV.CLIENT_URL, credentials:true}))
+app.use(clerkMiddleware());
 
 app.use("/api/inngest", serve({client:inngest, functions: functions}))
+app.use("/api/user", chatRoutes)
 
 
 app.get("/sliit", (req, res) => {
@@ -27,6 +32,8 @@ app.get("/sliit", (req, res) => {
 app.get("/it", (req, res) => {
     res.status(200).json({msg : "This is the data science endpoint"})
 });
+
+
 
 //make sliit-iq app ready for deployment
 if(ENV.NODE_ENV === "production"){
